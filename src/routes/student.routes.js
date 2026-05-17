2b1e79b920635eba644451070444382dc2dcb2ef
SatarupaBera <Satarupa.Bera@cognizant.com>
2026-05-17 11:03:18 +0530

    added enrollment apis and route

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getCourseById } from '../controller/course.controller.js';
-import { enrollment, showEnrolledCourses } from '../controller/enrollment.controller.js';
+import { enrollment, showEnrolledCourses, deleteEnrollment } from '../controller/enrollment.controller.js';
 import { getQuizById } from '../controller/quiz.controller.js';
 
 
@@ -10,6 +10,8 @@ const router = express.Router();
 router.route("/course/:courseId/enroll")
     .post(enrollment)
     .get(showEnrolledCourses)
+    
+router.delete("/course/delete",deleteEnrollment)
 
 router.route("/course/:courseId/quiz/:id")
     .get( getQuizById )
